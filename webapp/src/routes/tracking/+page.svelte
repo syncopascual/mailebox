ebc6091740e929a81b35b279d3cd39de7569b55c
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-05-10 19:50:18 +0800

    feat: make tracking page functional

--- webapp/src/routes/tracking/+page.svelte
+++ webapp/src/routes/tracking/+page.svelte
@@ -3,23 +3,20 @@
 	import Modal from '$lib/components/modal.svelte';
 	import StatusBar from '$lib/components/status_bar.svelte';
 	import StatusDetails from '$lib/components/status_details.svelte';
-	import { useConvexClient } from 'convex-svelte';
+	import { useConvexClient, useQuery } from 'convex-svelte';
 	import { api } from '$convex/_generated/api.js';
+	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { page } from '$app/state';
 
-	let {
-		tracking_num = 'RR123456785PH',
-		status = 'Sorting',
-		locker_num = '24',
-		deadline = '1'
-	} = $props();
-
+	// for expandable tracking details
 	let detailsOpen = $state(false);
 
 	function handleClose() {
 		detailsOpen = false;
 	}
 
+	// for QOL in entering OTP
 	let otpCode = $state(['', '', '', '', '', '']);
 	let isActive = $state(false);
 
@@ -33,9 +30,46 @@
 		}
 	}
 
+	// for fetching backend using tracking_num query
+	let tracking_num = $state(page.url.searchParams.get('tracking_num'));
+	let tracking_copy = $state(tracking_num);
+
+	if (tracking_num == null) tracking_num = '';
+
+	const userParcel = $derived(useQuery(api.parcels.getParcel, { tracking_num: tracking_num }));
+
+	let claim_in = 'N/A',
+		isClaimPeriodFinished = false,
+		days = $derived.by(() => {
+			if (userParcel.data != undefined) {
+				let claim_by = new Date(userParcel.data.parcel_info.claim_by);
+				let today = new Date(Date.now());
+
+				let hrs = Math.floor((claim_by - today) / 3600000);
+				let days = Math.floor(hrs / 24);
+
+				// Past claiming deadline already
+				if (hrs < 0) {
+					isClaimPeriodFinished = true;
+					days *= -1;
+					hrs *= -1;
+				}
+
+				return days;
+			}
+		});
+
+	// resubmit tracking number (updates url search parameters to match new submission)
+	function resubmitTrackingNumber() {
+		const params = new URLSearchParams(page.url.searchParams.toString())
+		params.set('tracking_num', tracking_num);
+		goto(`/tracking/?${params.toString()}`, { keepFocus: true, noScroll: true });
+	}
+
 	// Initialize the action hook
 	const client = useConvexClient();
 
+	For QR scanning (auth)
 	let otpStatus = $state('');
 	let isLoading = $state(false);
 	let currentScan = $state(null);
@@ -60,7 +94,7 @@
 		return Date.now() - scan.scanned_at * 1000 > threeMinutes;
 	}
 
-	async function handleButtonClick() {
+	async function submitOTP() {
 		isLoading = true;
 		otpStatus = '';
 		status = 'Authenticating via Python...';
@@ -120,46 +154,77 @@
 	>
 		<img src={logo} alt="MaiLeBox logo" class="mt-4 max-w-50" />
 		<div class="mt-4 flex w-full flex-col p-3">
-			<p class="py-1 text-left text-sm font-extrabold">Tracking Number</p>
-			<p class="bg-mlb-gray/50 text-mlb-black mt-2 rounded-3xl px-2 py-1.5 text-center font-bold">
-				{tracking_num}
-			</p>
+			{#if userParcel.data == undefined}
+				<p class="mb-4 text-center">
+					There is no userParcel with tracking number <span class="font-bold">{tracking_copy}</span>.
+				</p>
+				<div class="m-auto flex flex-col place-content-center">
+					<p class="w-full py-1 text-left text-lg font-extrabold">Input Tracking Number Again:</p>
+					<input
+						type="text"
+						bind:value={tracking_num}
+						class="bg-mlb-gray/50 text-mlb-black mt-2 rounded-3xl px-2 py-1.5 text-center font-bold"
+						oninput={resubmitTrackingNumber}
+					/>
+				</div>
+			{:else}
+				<p class="py-1 text-left text-sm font-extrabold">Tracking Number</p>
+				<p class="bg-mlb-gray/50 text-mlb-black mt-2 rounded-3xl px-2 py-1.5 text-center font-bold">
+					{tracking_num}
+				</p>
+			{/if}
 		</div>
 
-		<div class="grid w-full grid-cols-3 gap-2 gap-x-2 p-3">
-			<div class="text-sm font-bold">Status:</div>
-			<div class="col-span-2 text-sm">{status}</div>
+		{#if userParcel.isLoading}
+			Loading...
+		{:else if userParcel.error}
+			<p>failed to load: {userParcel.error.toString()}</p>
+		{:else if userParcel.data != undefined}
+			<div class="grid w-full grid-cols-3 gap-2 gap-x-2 p-3">
+				<div class="text-sm font-bold">Status:</div>
+				<div class="col-span-2 text-sm">{userParcel.data.parcel_info.status}</div>
 
-			<div class="text-sm font-bold">Parcel at:</div>
-			<div class="col-span-2 text-sm underline">#{locker_num}</div>
+				{#if userParcel.data.parcel_info.status == 'In Locker'}
+					<div class="text-sm font-bold">Parcel at:</div>
+					<div class="col-span-2 text-sm underline">#{userParcel.data.locker_num}</div>
 
-			<div class="text-sm font-bold">Claim in:</div>
-			<div class="col-span-2 text-sm">{deadline} days</div>
-		</div>
+					<div class="text-sm font-bold">Claim in:</div>
+					<div class="col-span-2 text-sm">
+						{#if isClaimPeriodFinished}
+							<span class="text-mlb-red">{days} days past claiming deadline</span>
+						{:else}
+							{days} days
+						{/if}
+					</div>
+				{/if}
+			</div>
 
-		<div class="py-2 md:w-4/5">
-			<!-- <p class="italic"> status tba </p> -->
-			<StatusBar stat={status} />
-		</div>
-		<button
-			onclick={() => (detailsOpen = true)}
-			class="bg-mlb-orange/80 hover:bg-mlb-orange my-2 w-1/3 rounded-full p-1"
-			aria-label="status dropdown"
-		>
-		</button>
+			<div class="py-2 md:w-4/5">
+				<!-- <p class="italic"> status tba </p> -->
+				<StatusBar stat={userParcel.data.parcel_info.status} />
+			</div>
+			<button
+				onclick={() => (detailsOpen = true)}
+				class="bg-mlb-orange/80 hover:bg-mlb-orange my-2 w-1/3 rounded-full p-1"
+				aria-label="status dropdown"
+			>
+			</button>
 
-		{#if detailsOpen}
-			<StatusDetails stat={status} onClose={handleClose} />
+			{#if detailsOpen}
+				<StatusDetails stat={userParcel.data.parcel_info.status} onClose={handleClose} />
+			{/if}
+
+			{#if userParcel.data.parcel_info.status == 'In Locker'}
+				<button
+					class="bg-mlb-orange text-mlb-white m-3 rounded-2xl px-4 py-1.5 text-sm font-medium drop-shadow-sm hover:brightness-90"
+					onclick={() => {
+						isActive = true;
+					}}
+				>
+					Unlock
+				</button>
+			{/if}
 		{/if}
-
-		<button
-			class="bg-mlb-orange text-mlb-white m-3 rounded-2xl px-4 py-1.5 text-sm font-medium drop-shadow-sm hover:brightness-90"
-			onclick={() => {
-				isActive = true;
-			}}
-		>
-			Unlock
-		</button>
 	</div>
 </div>
 
@@ -185,7 +250,7 @@
 
 		<button
 			class="bg-mlb-orange text-mlb-white text-l m-3 rounded-2xl px-7 py-3 font-medium drop-shadow-sm hover:brightness-90"
-			onclick={handleButtonClick}
+			onclick={submitOTP}
 			disabled={isLoading}
 		>
 			{isLoading ? 'Processing...' : 'Unlock'}
